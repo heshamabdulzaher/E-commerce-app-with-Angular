1b8fbb7ca8112846b5e5fa4f31d91085be16b0e8
hesham <heshamabdulzaher@gmail.com>
2018-10-27 17:37:34 +0200

    Shop page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdminDashboardComponent } from "./admin-dashboard/admin-dashboard.component";
+import { ShopPageComponent } from "./shop-page/shop-page.component";
 const routes: Routes = [
-  { path: "", component: AdminDashboardComponent }
-  // {path: "", component: AdminPanelComponent},
+  { path: "", component: ShopPageComponent },
+  { path: "admin", component: AdminDashboardComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const routingComponents = [AdminDashboardComponent];
+export const routingComponents = [AdminDashboardComponent, ShopPageComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-// Angular material
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule, MatCheckboxModule } from "@angular/material";
-import { MatInputModule } from "@angular/material/input";
 import { routingComponents, AppRoutingModule } from "./app-routing.module";
 // Components
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatInputModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
